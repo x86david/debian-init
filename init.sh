8a845acf30ed712fbcb91850e52a979f4260e081
David Pérez <91068383+x86david@users.noreply.github.com>
2026-03-13 13:16:10 +0100

    Simplify ZSH installation and package commands
    Refactor installation of ZSH environment and update package installation commands.

--- init.sh
+++ init.sh
@@ -22,8 +22,7 @@ apt update
 apt upgrade -y
 
 echo ">>> Installing essential utilities..."
-apt install -y sudo network-manager network-manager-gnome \
-    gnome-terminal gnome-text-editor git curl wget
+apt install -y sudo network-manager network-manager-gnome git curl wget
 
 echo ">>> Adding user '$USERNAME' to sudo group..."
 usermod -aG sudo "$USERNAME"
@@ -47,7 +46,8 @@ systemctl restart NetworkManager
 # --- Desktop selection ---
 if [ "$DESKTOP" = "gnome" ]; then
     echo ">>> Installing GNOME minimal (gnome-core)..."
-    apt install -y gnome-core gdm3
+    apt install -y gnome-core
+
 elif [ "$DESKTOP" = "xfce4" ]; then
     echo ">>> Installing XFCE4 minimal..."
     apt install -y xfce4 xfce4-goodies lightdm
@@ -57,22 +57,33 @@ fi
 
 # --- Install ZSH environment from GitHub ---
 echo ">>> Installing ZSH environment from GitHub..."
-sudo -u "$USERNAME" bash -c "
-    cd /home/$USERNAME
-    git clone https://github.com/x86david/install-zsh
-    cd install-zsh
-    chmod +x install-zsh.sh
-    sudo ./install-zsh.sh
-"
 
+sudo -u "$USERNAME" bash <<EOF
+cd /home/$USERNAME
+
+# Remove old clone if exists
+[ -d install-zsh ] && rm -rf install-zsh
+
+git clone https://github.com/x86david/install-zsh install-zsh
+cd install-zsh
+
+chmod +x install-zsh.sh
+
+echo "ℹ️  Only users who currently use /bin/bash will be switched to Zsh."
+
+# Patch installer to change shell ONLY for users with /bin/bash
+sed -i 's/for u in .*/for u in \$(awk -F: '\''\$7 == "\/bin\/bash" {print \$1}'\'' \/etc\/passwd); do/' install-zsh.sh
+
+./install-zsh.sh
+EOF
+
+# --- Configure GRUB to use simple console mode ---
 echo ">>> Configuring GRUB to use simple console mode..."
 
-# Remove conflicting lines
 sed -i '/GRUB_GFXMODE/d' /etc/default/grub
 sed -i '/GRUB_GFXPAYLOAD_LINUX/d' /etc/default/grub
 sed -i '/GRUB_TERMINAL/d' /etc/default/grub
 
-# Add only the console terminal setting
 cat <<EOF >> /etc/default/grub
 
 # Simple, clean GRUB console mode
@@ -81,7 +92,6 @@ EOF
 
 echo ">>> Updating GRUB..."
 update-grub
-
 
 echo ">>> Cleaning up..."
 apt autoremove -y
